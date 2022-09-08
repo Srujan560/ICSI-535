fd2dec3caf20b3724f7caf011a74381e22db2671
Srujan <Srujan560@gmail.com>
2022-09-07 23:11:25 -0400

    Keeping Back up

--- DFS_undirected graph_Unweighted.py
+++ DFS_undirected graph_Unweighted.py
@@ -28,29 +28,38 @@ visted ={'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'P':0,'Q':0,'R':0}
 # print(visted)
 q= deque()
 ans=[]
-q.append(list(g1.keys())[0])
+q.append(list(g1.keys())[0])# First Let add our Start node 'S' in our deque
+#we will solve dfs using recursion 
+"""
+First We check if q is empty
+than we will pop the leftmost or FIFO
+Than We sill if it is 'G' our target to find Goal
+than go through each Node that is neighbors
+than add the first neighbor we visit
+than recursively call our function if not found it will come back to the for loop else it will return path it took 
+"""
 def dfs(visted,q=deque(),ans=[]):
     if not q:
-        return ans
+        return ans# return an list
     temp = q.popleft()
     ans.append(temp)
-    if(temp=='G'):
+    if(temp=='M'):
         str_ans_path =""
         for x in ans:
             str_ans_path=str_ans_path+x+" "
         print("DFS",str_ans_path)
-        ans =str_ans_path
-        return ans
+        ans =str_ans_path #This is break our for loop
+        return ans # we return string
         # exit()
     visted[temp]=1
     print(visted)
     for x in g1[temp]:
-        if visted[x]==0:
-            q.append(x)
-            ans = dfs(visted,q,ans)
-            if isinstance(ans,str):
-                break
-    return ans
+        if visted[x]==0:#Only visit a unvisited node
+            q.append(x)# add to the deque
+            ans = dfs(visted,q,ans)# here we store answer
+            if isinstance(ans,str): # check if it is an answer
+                break#than exit the for loop
+    return ans#Just return the same string till nothing left
 t =dfs(visted,q,ans)   
 # print(t)
 
