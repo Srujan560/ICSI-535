fb67eb0936155a6f2ea2bb8fd157b878d623b9d8
Srujan <Srujan560@gmail.com>
2022-09-09 21:01:10 -0400

    Add all where can run all codes in one place

---
+++ All Main.py
@@ -0,0 +1,21 @@
+# import "bfs_undirected_graph vextex recursive.py"
+# import 
+# bfs_vextex_recursive = __import__("")
+# bfs_vextex_stack = __import__("")
+# bfs_adjacent_matrix_recursive = __import__("")
+# bfs_adjacent_matrix_stack = __import__("")
+
+# dfs_vextex_recursive = __import__("")
+# dfs_vextex_stack = __import__("")
+# dfs_adjacent_matrix_recursive = __import__("")
+# dfs_adjacent_matrix_stack = __import__("")
+
+bfs_vextex_recursive = __import__("BFS_undirected_graph Vextex recureseive")
+bfs_vextex_stack = __import__("BFS_undirected_graph Vextex stack")
+bfs_adjacent_matrix_recursive =__import__("BFS_undirected_graph adjacent matrix recurcive")
+bfs_adjacent_matrix_stack = __import__("BFS_undirected_graph adjacent matrix stack")
+
+dfs_vextex_recursive = __import__("DFS_undirected graph_Unweighted")
+dfs_vextex_stack = __import__("DFS_Undirected graph_unweighted_stack")
+dfs_adjacent_matrix_recursive =__import__("DFS_undirected_graph adjacent_matrix recuresion")
+dfs_adjacent_matrix_stack =__import__("DFS_undirected_graph adjacent_matrix stack")
