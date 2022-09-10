3b36a19aecf0439c01afd87d09433b1a95753b3d
Srujan <Srujan560@gmail.com>
2022-09-09 22:40:45 -0400

    all g2 is added to main.py

--- All Main.py
+++ All Main.py
@@ -19,3 +19,13 @@ dfs_vextex_recursive = __import__("DFS_undirected graph_Unweighted")
 dfs_vextex_stack = __import__("DFS_Undirected graph_unweighted_stack")
 dfs_adjacent_matrix_recursive =__import__("DFS_undirected_graph adjacent_matrix recuresion")
 dfs_adjacent_matrix_stack =__import__("DFS_undirected_graph adjacent_matrix stack")
+
+bfs_vextex_recursive_G2 = __import__("G2 BFS Vextex recursive")
+bfs_vextex_stack_G2 = __import__("G2 BFS Vextex stack")
+bfs_adjacent_matrix_recursive_G2 = __import__("G2 BFS adjacent matrix recursive")
+bfs_adjacent_matrix_stack_G2 = __import__("G2 BFS adjacent matrix  stack")
+
+dfs_vextex_recursive_G2 = __import__("G2 DFS Vextex recursive")
+dfs_vextex_stack_G2 = __import__("G2 DFS vextex stack")
+dfs_adjacent_matrix_recursive_G2 = __import__("G2 DFS adjacent matrix recursive")
+dfs_adjacent_matrix_stack_G2 = __import__("G2 DFS adjacent matrix stack")
