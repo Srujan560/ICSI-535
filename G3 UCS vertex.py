9856e917ab6a713da49cb13e65d02432e1b65792
Srujan <Srujan560@gmail.com>
2022-09-13 19:50:24 -0400

    Update right out put and chage UCF algorthim

--- G3 UCS vertex.py
+++ G3 UCS vertex.py
@@ -16,6 +16,33 @@ g3= {
 
 }
 visited ={'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'P':0,'Q':0,'R':0,'S':0}
+
+p_queue = queue.PriorityQueue() #
+p_queue.put((0,'S',['S']))# In here We put current cost Current Node , and empty list
+ans =[]
+while not p_queue.empty():
+    print(p_queue.queue)
+    temp= p_queue.get()# let get lowest cost 
+    visited[temp[1]]=1# update Graph 
+    if temp[1]=='G':# stop once we find cheap 
+        # temp[3].append(temp[1])
+        ans=temp[2]# return the list
+        ans.append(temp[0])# add the cost
+        p_queue.task_done()# empty the stack
+        break
+    else:
+        for x  in g3[temp[1]]:
+            if visited[x]==0:# make sure it is not visited 
+                t3=list(temp[2])
+                t3.append(x)
+                # here we add all the adjacent neighbor and the cost ... only lost cost will run  
+                p_queue.put((temp[0]+g3[temp[1]][x],x,t3))
+# print(ans)
+cost = ans.pop()
+temp_str="UCS g3 Cost "+str(cost)+" "
+for x in ans:
+    temp_str = temp_str +x+" "
+print(temp_str)
 # test = ((4,['A','B','C']),(11,['a','c','f']))
 # temp = list(test)
 # temp.append((6,['a','x','z']))
@@ -27,155 +54,155 @@ visited ={'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'P':0,'Q':0,'R':0,'S':
 # print(test)
 # test=sorted(test, reverse=True)
 # print(test)
-p_queue = queue.PriorityQueue()
-p_queue.put((0,'S'))#let add stating node
-ans = ["S"]*len(g3['S'])
-possible1=[0,'S']
-possible2=[0,'S']
-possible3=[0,'S']
+# p_queue = queue.PriorityQueue()
+# p_queue.put((0,'S'))#let add stating node
+# ans = ["S"]*len(g3['S'])
+# possible1=[0,'S']
+# possible2=[0,'S']
+# possible3=[0,'S']
 
-# ans[2] = ['G']
-# ans[1]=['S']*10
-# if 'G' in ans[2]:
-#     print("LOLOLO")
-# print(ans)
-test =[]
-while not p_queue.empty():
-    print(p_queue.queue)
-    temp = p_queue.get()
-    if temp[0]==0:
-        for x,val in enumerate(g3[temp[1]]):
-            if x==0:
-                # possible1[0]=g3[temp[1]][val]
-                # possible1.append(val)
-                p_queue.put((g3[temp[1]][val],val,val))
-            if x==1:
-                # possible2[0]=g3[temp[1]][val]
-                # possible2.append(val)
-                p_queue.put((g3[temp[1]][val],val,val))
-            if x==2:
-                # possible3[0]=g3[temp[1]][val]
-                # possible3.append(val)
-                p_queue.put((g3[temp[1]][val],val,val))
-    else:
-        check= 0
-        # print(possible1[-1],"****",list(g3[temp[1]].keys()))
-        # print(temp[1],"^^^^",list(g3[possible1[-1]].keys()))
-        # break
-        for x in g3[temp[1]]:
-            # print(list(g3[possible1[-1]].keys()),x ,"*******")
+# # ans[2] = ['G']
+# # ans[1]=['S']*10
+# # if 'G' in ans[2]:
+# #     print("LOLOLO")
+# # print(ans)
+# test =[]
+# while not p_queue.empty():
+#     print(p_queue.queue)
+#     temp = p_queue.get()
+#     if temp[0]==0:
+#         for x,val in enumerate(g3[temp[1]]):
+#             if x==0:
+#                 # possible1[0]=g3[temp[1]][val]
+#                 # possible1.append(val)
+#                 p_queue.put((g3[temp[1]][val],val,val))
+#             if x==1:
+#                 # possible2[0]=g3[temp[1]][val]
+#                 # possible2.append(val)
+#                 p_queue.put((g3[temp[1]][val],val,val))
+#             if x==2:
+#                 # possible3[0]=g3[temp[1]][val]
+#                 # possible3.append(val)
+#                 p_queue.put((g3[temp[1]][val],val,val))
+#     else:
+#         check= 0
+#         # print(possible1[-1],"****",list(g3[temp[1]].keys()))
+#         # print(temp[1],"^^^^",list(g3[possible1[-1]].keys()))
+#         # break
+#         for x in g3[temp[1]]:
+#             # print(list(g3[possible1[-1]].keys()),x ,"*******")
             
-            if temp[2]=='D':# if D
-                if check ==0 and temp[1] in list(g3[possible1[-1]].keys()) and temp[1] not in possible1:
-                    check=1
-                    possible1[0]= possible1[0]+temp[0]
-                    possible1.append(temp[1])
+#             if temp[2]=='D':# if D
+#                 if check ==0 and temp[1] in list(g3[possible1[-1]].keys()) and temp[1] not in possible1:
+#                     check=1
+#                     possible1[0]= possible1[0]+temp[0]
+#                     possible1.append(temp[1])
             
-                if x not in possible1 and 'G' not in possible1 and 'STOP' not in possible1 and (g3[temp[1]][x],x,'D') not in list(p_queue.queue):
-                    # possible1[0]= possible1[0]+g3[temp[1]][x]
-                    # possible1.append(x)
-                    if x in list(g3[possible1[-1]].keys()):
-                        p_queue.put((g3[temp[1]][x],x,'D'))
-            if temp[2]=='E' and 'STOP' not in possible2:
-                if check ==0 and temp[1] in list(g3[possible2[-1]].keys()) and temp[1] not in possible2:
-                    check=1
-                    possible2[0]= possible2[0]+temp[0]
-                    possible2.append(temp[1])
+#                 if x not in possible1 and 'G' not in possible1 and 'STOP' not in possible1 and (g3[temp[1]][x],x,'D') not in list(p_queue.queue):
+#                     # possible1[0]= possible1[0]+g3[temp[1]][x]
+#                     # possible1.append(x)
+#                     if x in list(g3[possible1[-1]].keys()):
+#                         p_queue.put((g3[temp[1]][x],x,'D'))
+#             if temp[2]=='E' and 'STOP' not in possible2:
+#                 if check ==0 and temp[1] in list(g3[possible2[-1]].keys()) and temp[1] not in possible2:
+#                     check=1
+#                     possible2[0]= possible2[0]+temp[0]
+#                     possible2.append(temp[1])
              
-                if x not in possible2 and 'G' not in possible2 and 'STOP' not in possible2 and (g3[temp[1]][x],x,'E') not in list(p_queue.queue):
-                    # possible2[0]= possible2[0]+g3[temp[1]][x]
-                    # possible2.append(x)
-                    if x in list(g3[possible2[-1]].keys()):
-                        p_queue.put((g3[temp[1]][x],x,'E'))
-            if temp[2]=='P':
-                if check ==0 and temp[1] in list(g3[possible3[-1]].keys()) and temp[1] not in possible3:
-                    check=1
-                    possible3[0]= possible3[0]+temp[0]
-                    possible3.append(temp[1])
-                #let make sure it not the PQ already 
-                if x not in possible3 and 'STOP' not in possible3 and 'G' not in possible3 and (g3[temp[1]][x],x,'P') not in list(p_queue.queue):
-                    # possible3[0]= possible3[0]+g3[temp[1]][x]
-                    # possible3.append(x)
-                    if x in list(g3[possible3[-1]].keys()):# this checks that last index can access Node before we put in the p q
-                        p_queue.put((g3[temp[1]][x],x,'P'))
-    print(possible1,"p1")
-    print(possible2,"p2")
-    print(possible3,"p3",)
-    # time.sleep(1.5)
-    if 'G' in possible1 or 'G' in possible2 or 'G' in possible3:
-        # break
-        if not test:
-            # print("test ", test)
-            if 'G' in possible1:
-                test=possible1
-                # print("Test",test,"p1",possible1)
-                # break
+#                 if x not in possible2 and 'G' not in possible2 and 'STOP' not in possible2 and (g3[temp[1]][x],x,'E') not in list(p_queue.queue):
+#                     # possible2[0]= possible2[0]+g3[temp[1]][x]
+#                     # possible2.append(x)
+#                     if x in list(g3[possible2[-1]].keys()):
+#                         p_queue.put((g3[temp[1]][x],x,'E'))
+#             if temp[2]=='P':
+#                 if check ==0 and temp[1] in list(g3[possible3[-1]].keys()) and temp[1] not in possible3:
+#                     check=1
+#                     possible3[0]= possible3[0]+temp[0]
+#                     possible3.append(temp[1])
+#                 #let make sure it not the PQ already 
+#                 if x not in possible3 and 'STOP' not in possible3 and 'G' not in possible3 and (g3[temp[1]][x],x,'P') not in list(p_queue.queue):
+#                     # possible3[0]= possible3[0]+g3[temp[1]][x]
+#                     # possible3.append(x)
+#                     if x in list(g3[possible3[-1]].keys()):# this checks that last index can access Node before we put in the p q
+#                         p_queue.put((g3[temp[1]][x],x,'P'))
+#     print(possible1,"p1")
+#     print(possible2,"p2")
+#     print(possible3,"p3",)
+#     # time.sleep(1.5)
+#     if 'G' in possible1 or 'G' in possible2 or 'G' in possible3:
+#         # break
+#         if not test:
+#             # print("test ", test)
+#             if 'G' in possible1:
+#                 test=possible1
+#                 # print("Test",test,"p1",possible1)
+#                 # break
                 
-            elif 'G' in possible2:
-                test=possible2
-            else:
-                test=possible3
-        else:
-            # print(temp[2]," **** ",test)
-            if temp[2]=='D' and 'D' != test[2]:
-                # print(temp,"****",test)
-                # break
-                if test[0] < possible1[0] and 'STOP' not in possible1:
-                    possible1.append('STOP')
-                else:
-                    if 'G' in possible1:
-                        test=possible1
-            elif temp[2]=='E' and 'E' != test[2]:
-                # print(test[0],"****",possible2[0])
-                # break
-                if test[0] < possible2[0] and 'STOP' not in possible2:
-                    possible2.append('STOP')
-                else:
-                    if 'G' in possible2:
-                        test=possible2
-            elif temp[2]=='P'and 'P' != test[2]:
-                if test[0] < possible3[0] and 'STOP' not in possible3:
-                    possible3.append('STOP')
-                else:
-                    if 'G' in possible3:
-                        test=possible3
-    if 'STOP' in possible2 and 'STOP' in possible3 and 'G' in possible1:
-        break
-    if 'STOP' in possible1 and 'STOP' in possible2 and 'G' in possible3:
-        break 
-    if 'STOP' in possible1 and 'STOP' in possible3 and 'G' in possible2:
-        break  
-    print(test,"Test\n")
+#             elif 'G' in possible2:
+#                 test=possible2
+#             else:
+#                 test=possible3
+#         else:
+#             # print(temp[2]," **** ",test)
+#             if temp[2]=='D' and 'D' != test[2]:
+#                 # print(temp,"****",test)
+#                 # break
+#                 if test[0] < possible1[0] and 'STOP' not in possible1:
+#                     possible1.append('STOP')
+#                 else:
+#                     if 'G' in possible1:
+#                         test=possible1
+#             elif temp[2]=='E' and 'E' != test[2]:
+#                 # print(test[0],"****",possible2[0])
+#                 # break
+#                 if test[0] < possible2[0] and 'STOP' not in possible2:
+#                     possible2.append('STOP')
+#                 else:
+#                     if 'G' in possible2:
+#                         test=possible2
+#             elif temp[2]=='P'and 'P' != test[2]:
+#                 if test[0] < possible3[0] and 'STOP' not in possible3:
+#                     possible3.append('STOP')
+#                 else:
+#                     if 'G' in possible3:
+#                         test=possible3
+#     if 'STOP' in possible2 and 'STOP' in possible3 and 'G' in possible1:
+#         break
+#     if 'STOP' in possible1 and 'STOP' in possible2 and 'G' in possible3:
+#         break 
+#     if 'STOP' in possible1 and 'STOP' in possible3 and 'G' in possible2:
+#         break  
+#     print(test,"Test\n")
 
-        # break
+#         # break
     
-    # print(temp[1],"Test")
-    # if visited[temp[1]]==0:
-    #     visited[temp[1]]=1
-    #     for index,val in enumerate(g3[temp[1]]):
-    #         print(index," val = ",val)
-    #         # if visited[val]==0:
-    #         # ans[index] = ans[index]+" "+val
-    #         num = temp[0]+g3[temp[1]][val]
-    #         # val=val
-    #         # print(val)
-    #         # temp[2]=list(temp[2]).append(temp[1])
-    #         # print(temp[2],"******")
+#     # print(temp[1],"Test")
+#     # if visited[temp[1]]==0:
+#     #     visited[temp[1]]=1
+#     #     for index,val in enumerate(g3[temp[1]]):
+#     #         print(index," val = ",val)
+#     #         # if visited[val]==0:
+#     #         # ans[index] = ans[index]+" "+val
+#     #         num = temp[0]+g3[temp[1]][val]
+#     #         # val=val
+#     #         # print(val)
+#     #         # temp[2]=list(temp[2]).append(temp[1])
+#     #         # print(temp[2],"******")
 
-    #         p_queue.put((num,val))
+#     #         p_queue.put((num,val))
 
         
-        # test =[x for x in ans if 'G' in x]
-        # if test:
-        #     temp_str= "UCS "+str(test)+" cost: "+str(temp[0])
-        #     print(temp_str)
-        # else:
-        # print(ans)
-    # print(temp)
-temp_str ="UCS cost "+str(test[0])+" "
-for index,val in enumerate(test):
-    if index !=0:
-        temp_str = temp_str+val+" "
-print(temp_str)
+#         # test =[x for x in ans if 'G' in x]
+#         # if test:
+#         #     temp_str= "UCS "+str(test)+" cost: "+str(temp[0])
+#         #     print(temp_str)
+#         # else:
+#         # print(ans)
+#     # print(temp)
+# temp_str ="UCS cost "+str(test[0])+" "
+# for index,val in enumerate(test):
+#     if index !=0:
+#         temp_str = temp_str+val+" "
+# print(temp_str)
 
     
--- G4 adjacent matrix.py
+++ G4 adjacent matrix.py
@@ -15,15 +15,47 @@ g4 =[
 
 ]
 maps ={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'P',9:'Q',10:'R',11:'S'}
+
+visited = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0}
 # we know 3 possible paths from the starting node
-paths = [
-    [],
-    [],
-    []
-]
+
 priority_q = queue.PriorityQueue()
-priority_q.put((0,11))#Let put our starting Node
+priority_q.put((0,11,[11]))#Let put our starting Node
 ans= []
+while not priority_q.empty():
+    print(priority_q.queue)
+    temp = priority_q.get()
+    visited[temp[1]]=1
+    if temp[1]==6:
+        ans=temp[2]
+        # ans.append(temp[1])
+        ans.append(temp[0])
+        priority_q.task_done()
+        break
+    # print(g4[temp[1]])
+    for index,val in enumerate(g4[temp[1]]):
+        # print(index,"**",val)
+        if val !=0 and visited[index]==0:# make sure it an adjacent neighbor and not been Visisted[index]
+            temp_list = list(temp[2])
+            temp_list.append(index)
+            priority_q.put((temp[0]+val,index,temp_list))
+
+print(ans)
+temp_str = "UCS cost "+str(ans[-1])+" and path "
+ans.pop()# remove our cost 
+for x in ans:
+    if x <12:
+        temp_str = temp_str+maps[x]+" "
+print(temp_str)
+# # we know 3 possible paths from the starting node
+# paths = [
+#     [],
+#     [],
+#     []
+# ]
+# priority_q = queue.PriorityQueue()
+# priority_q.put((0,11))#Let put our starting Node
+# ans= []
 """
 first if starting node let add the first 3 paths in each one speared paths (paths[0], paths[1], paths[2])
 Each list in paths will remember the total cost and order they picked the edge
@@ -34,101 +66,101 @@ Once we find our first 6 (Goal) and cost than we check if other paths have more 
 we update our new ans to be the lowest cost
 """
 # history_queue=[]
-while  not priority_q.empty():
-    print(priority_q.queue, "Priority queue")
-    temp = priority_q.get()
-    if temp[0]==0:# if staring node
-        for index,val in enumerate(g4[temp[1]]):
-            # print(index,"***",val)
-            if val!=0:
-                for x,path in enumerate(paths):# loop through it add first index and take paths.. we 3 paths are so each node take a paths index
-                    # print(len(path))
-                    if len(path)==0:
-                        paths[x].append(val)# add the cost to our paths[0]array
-                        paths[x].append(temp[1])# than stating node 
-                        paths[x].append(index)# than node that about in the queue
-                        priority_q.put((val,index,x))
-                        break
+# while  not priority_q.empty():
+#     print(priority_q.queue, "Priority queue")
+#     temp = priority_q.get()
+#     if temp[0]==0:# if staring node
+#         for index,val in enumerate(g4[temp[1]]):
+#             # print(index,"***",val)
+#             if val!=0:
+#                 for x,path in enumerate(paths):# loop through it add first index and take paths.. we 3 paths are so each node take a paths index
+#                     # print(len(path))
+#                     if len(path)==0:
+#                         paths[x].append(val)# add the cost to our paths[0]array
+#                         paths[x].append(temp[1])# than stating node 
+#                         paths[x].append(index)# than node that about in the queue
+#                         priority_q.put((val,index,x))
+#                         break
             
-                # print(x,"****",path,"\n")
-    else:
-        #and g4[paths[temp[2][-1]]][temp[1]]!=0
-        num = g4[paths[temp[2]][-1]][temp[1]]# this so make sure it is node that is adjacent
-        # print(num)
-        if -1  not in paths[temp[2]] and temp[1] not in paths[temp[2]] and 6!= paths[temp[2]][-1] and num !=0:#checks matrix if does have -1 and no copies
-            paths[temp[2]].append(temp[1])#add our index
-            paths[temp[2]][0] = paths[temp[2]][0]+temp[0]#update the cost
-            if temp[1] == 6 and not ans:# make sure it first to reach goal 
-                ans = paths[temp[2]]
-        for index,val in enumerate(g4[temp[1]]):# loop to find and queue next nodes
-            if temp[2]==0:
-                if index not in paths[0] and val!=0 and paths[0][-1]!=6 and -1 not in paths[0]:# we do not want queue if cost more than current answer and not go after index 6
-                    # paths[0][0]= paths[0][0]+val
-                    # paths[0].append(index)
-                    # if index ==6:
-                    #     paths[0].append('GOAL')
-                    # if index ==6 and  not ans:
-                    #     ans=paths[0]
-                    #     priority_q.put((val,index,0))
-                    #     # paths[1].append('GOAL')
-                    # else:
-                    # print(index,"***",g4[paths[temp[2]][-1]])
-                    if (val,index,0) not in list(priority_q.queue) and  g4[paths[temp[2]][-1]][index ]!=0:
-                        priority_q.put((val,index,0))# cost, index , which path it belongs to
-                        # history_queue.append((val,index,0))
+#                 # print(x,"****",path,"\n")
+#     else:
+#         #and g4[paths[temp[2][-1]]][temp[1]]!=0
+#         num = g4[paths[temp[2]][-1]][temp[1]]# this so make sure it is node that is adjacent
+#         # print(num)
+#         if -1  not in paths[temp[2]] and temp[1] not in paths[temp[2]] and 6!= paths[temp[2]][-1] and num !=0:#checks matrix if does have -1 and no copies
+#             paths[temp[2]].append(temp[1])#add our index
+#             paths[temp[2]][0] = paths[temp[2]][0]+temp[0]#update the cost
+#             if temp[1] == 6 and not ans:# make sure it first to reach goal 
+#                 ans = paths[temp[2]]
+#         for index,val in enumerate(g4[temp[1]]):# loop to find and queue next nodes
+#             if temp[2]==0:
+#                 if index not in paths[0] and val!=0 and paths[0][-1]!=6 and -1 not in paths[0]:# we do not want queue if cost more than current answer and not go after index 6
+#                     # paths[0][0]= paths[0][0]+val
+#                     # paths[0].append(index)
+#                     # if index ==6:
+#                     #     paths[0].append('GOAL')
+#                     # if index ==6 and  not ans:
+#                     #     ans=paths[0]
+#                     #     priority_q.put((val,index,0))
+#                     #     # paths[1].append('GOAL')
+#                     # else:
+#                     # print(index,"***",g4[paths[temp[2]][-1]])
+#                     if (val,index,0) not in list(priority_q.queue) and  g4[paths[temp[2]][-1]][index ]!=0:
+#                         priority_q.put((val,index,0))# cost, index , which path it belongs to
+#                         # history_queue.append((val,index,0))
 
-            if temp[2]==1:
-                if index not in paths[1] and val!=0 and paths[1][-1]!=6 and -1  not in paths[1]:
-                    # paths[1][0]= paths[1][0]+val
-                    # paths[1].append(index)
-                    # if index ==6 and  not ans:
-                    #     ans=paths[1]
-                    #     # paths[1].append('GOAL')
-                    # else:
-                    if (val,index,1) not in list(priority_q.queue) and g4[paths[temp[2]][-1]][index ]!=0:
-                        priority_q.put((val,index,1))
-            if temp[2]==2:
-                if index not in paths[2] and val!=0 and paths[2][-1]!=6  and -1 not in  paths[2]:
-                    # paths[2][0]= paths[2][0]+val
-                    # paths[2].append(index)
-                    # if index ==6:
-                    #     paths[2].append('GOAL')
-                    # if index ==6 and  not ans:
-                    #     ans=paths[2]
-                    #     # paths[1].append('GOAL')
-                    # else:
-                    if (val,index,2) not in list(priority_q.queue) and g4[paths[temp[2]][-1]][index ]!=0:
-                        priority_q.put((val,index,2))
+#             if temp[2]==1:
+#                 if index not in paths[1] and val!=0 and paths[1][-1]!=6 and -1  not in paths[1]:
+#                     # paths[1][0]= paths[1][0]+val
+#                     # paths[1].append(index)
+#                     # if index ==6 and  not ans:
+#                     #     ans=paths[1]
+#                     #     # paths[1].append('GOAL')
+#                     # else:
+#                     if (val,index,1) not in list(priority_q.queue) and g4[paths[temp[2]][-1]][index ]!=0:
+#                         priority_q.put((val,index,1))
+#             if temp[2]==2:
+#                 if index not in paths[2] and val!=0 and paths[2][-1]!=6  and -1 not in  paths[2]:
+#                     # paths[2][0]= paths[2][0]+val
+#                     # paths[2].append(index)
+#                     # if index ==6:
+#                     #     paths[2].append('GOAL')
+#                     # if index ==6 and  not ans:
+#                     #     ans=paths[2]
+#                     #     # paths[1].append('GOAL')
+#                     # else:
+#                     if (val,index,2) not in list(priority_q.queue) and g4[paths[temp[2]][-1]][index ]!=0:
+#                         priority_q.put((val,index,2))
                        
-                    # priority_q.put((val,index,2))
-    print(paths[0],"path 1")
-    print(paths[1],"path 2")
-    print(paths[2],"path 3\n")
+#                     # priority_q.put((val,index,2))
+#     print(paths[0],"path 1")
+#     print(paths[1],"path 2")
+#     print(paths[2],"path 3\n")
 
-                    # break
-    # if paths[0][-1]==6 or paths[1][-1]==6 or paths[2][-1]==6:
-    #     pass
-    if ans:# to make sure we stop asking if paths cost more than current
-        if ans[2]!=paths[0][2] and ans[0]<paths[0][0] and -1 not in paths[0]:
-            paths[0].append(-1)
-        if ans[2]!=paths[1][2] and ans[0]<paths[1][0] and -1 not in paths[1]:
-            paths[1].append(-1)
-        if ans[2]!=paths[2][2] and ans[0]<paths[2][0] and -1 not in paths[2]:
-            paths[2].append(-1)
-        # stop code if we have two -1s and 6 than stop the code
-        if paths[0][-1]==6 and paths[1][-1]==-1 and paths[2][-1]==-1:
-            break
-        if paths[1][-1]==6 and paths[0][-1]==-1 and paths[2][-1]==-1:
-            break
-        if paths[2][-1]==6 and paths[1][-1]==-1 and paths[0][-1]==-1:
-            break
-print(priority_q.queue, "Priority queue")
-print(paths[0],"path 1")
-print(paths[1],"path 2")
-print(paths[2],"path 3\n")
-# for printing 
-temp_str = "UCS cost "+str(ans[0])+" and path "
-for x in ans:
-    if x <12:
-        temp_str = temp_str+maps[x]+" "
-print(temp_str)
+#                     # break
+#     # if paths[0][-1]==6 or paths[1][-1]==6 or paths[2][-1]==6:
+#     #     pass
+#     if ans:# to make sure we stop asking if paths cost more than current
+#         if ans[2]!=paths[0][2] and ans[0]<paths[0][0] and -1 not in paths[0]:
+#             paths[0].append(-1)
+#         if ans[2]!=paths[1][2] and ans[0]<paths[1][0] and -1 not in paths[1]:
+#             paths[1].append(-1)
+#         if ans[2]!=paths[2][2] and ans[0]<paths[2][0] and -1 not in paths[2]:
+#             paths[2].append(-1)
+#         # stop code if we have two -1s and 6 than stop the code
+#         if paths[0][-1]==6 and paths[1][-1]==-1 and paths[2][-1]==-1:
+#             break
+#         if paths[1][-1]==6 and paths[0][-1]==-1 and paths[2][-1]==-1:
+#             break
+#         if paths[2][-1]==6 and paths[1][-1]==-1 and paths[0][-1]==-1:
+#             break
+# print(priority_q.queue, "Priority queue")
+# print(paths[0],"path 1")
+# print(paths[1],"path 2")
+# print(paths[2],"path 3\n")
+# # for printing 
+# temp_str = "UCS cost "+str(ans[0])+" and path "
+# for x in ans:
+#     if x <12:
+#         temp_str = temp_str+maps[x]+" "
+# print(temp_str)
