8a5401c32b731a954c39544a66aed035320fcb4c
Srujan <Srujan560@gmail.com>
2022-09-17 19:35:33 -0400

    BFS stack return path

--- G2 BFS Vextex stack.py
+++ G2 BFS Vextex stack.py
@@ -16,23 +16,31 @@ ans=[]
 stack_bfs=[]
 stack_bfs.append(list(g2.keys())[0])
 temp_str=""
+current_stack=""
 while stack_bfs and temp_str!='G':#Let make sure our stack is not empty or our Node to ='G'
     print("Stack_BFS",stack_bfs)
-    temp_str=stack_bfs.pop()
+    current_stack=stack_bfs.pop()
+    temp_str=current_stack[-1]
     if visited[temp_str]==0: # Let check if visited or not
         visited[temp_str]=1
         ans.append(temp_str)
         temp_stack=[]# we create a temp stack to put current stack at top, so when we pop we pop at level not depth 
         for x in reversed(g2[temp_str]):#We loop in revers so we put most right at bottom of stack 
-            if visited[x]==0 and x  not in stack_bfs: # make sure it has not been visited yet, and not in stack already 
-                temp_stack.append(x)
+            if visited[x]==0: #and x  not in stack_bfs: # make sure it has not been visited yet, and not in stack already 
+                new_stack = list(current_stack)
+                new_stack.append(x)
+                # print(new_stack)
+                
+                temp_stack.append(new_stack)
+        
         # print("temp_stack",temp_stack)
         if temp_stack:# If we Nodes to add to current stack
             for x in (stack_bfs):# puts the currrent node stacks at top
                 temp_stack.append(x)
             stack_bfs=temp_stack # return newst nodes at which order to go
     # print("End Loop")
-print("ans BFS",ans) 
+print("BFS expanded",ans) 
+print("BFS PATH",current_stack)
    
             
 
