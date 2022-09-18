de2b45c1b4d2b8e1e7e05baa28de5aae5ca25d13
Srujan <Srujan560@gmail.com>
2022-09-17 20:07:38 -0400

    adjacent matrix stack with return path

--- G2 BFS adjacent matrix  stack.py
+++ G2 BFS adjacent matrix  stack.py
@@ -16,28 +16,38 @@ maps ={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'P',9:'Q',10:'R',11:'S'
 visited =[0]*12
 ans=[]
 stack_bfs=[]
-stack_bfs.append(11)# let add our staring index (node)
+stack_bfs.append([11])# let add our staring index (node)
 temp_num=0
 """
 let have while loop that run till stack is not empty and current pop is not 6
 Check if current node is not been visited and loop through and get adjacent neighbors 
 then put in stack where left most at top of stack so when pop it goes through level of adjacent matrix 
 """
+current_stack=[]
 while stack_bfs and temp_num!=6:
     print(stack_bfs)
-    temp_num=stack_bfs.pop()
+    current_stack=stack_bfs.pop()
+    temp_num=current_stack[-1]
+    if temp_num==6:
+        break
     if visited[temp_num]==0:#let check and update our visited nodes
         visited[temp_num]=1
         ans.append(temp_num)
         temp_stack=[]# this stack make sure that we have left most stack at top of our stack
         for index,val in reversed(list(enumerate(g2[temp_num]))):# going reversed b/c we get right most neighbor to be bottom of our stack and left most at top our stack
-            if val==1 and visited[index]==0 and index not in stack_bfs:# make sure it is a neighbor and not been visited not it is the stack
-                temp_stack.append(index)
+            if val==1 and visited[index]==0: #and index not in stack_bfs:# make sure it is a neighbor and not been visited not it is the stack
+                new_stack=list(current_stack)
+                new_stack.append(index)
+                temp_stack.append(new_stack)
         for x in stack_bfs:#loop add current stack and right to left at top of the sack
             temp_stack.append(x)
         stack_bfs=temp_stack# give the update stack
 #for printing purpose
-temp_str="BFS "
+temp_str="BFS expanded "
 for x in ans:
     temp_str =temp_str+maps[x]+" "
+print(temp_str)
+temp_str= "BFS PATH "
+for x in current_stack:
+    temp_str= temp_str+maps[x]+" "
 print(temp_str)
